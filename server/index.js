20526293ce01236cc4e3b909549bac14a59b67f7
nathan-08 <nathanryan001@gmail.com>
2018-10-10 19:12:57 -0600

    after hours done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 1337;
+let json;
 
 app.use(bodyParser.json());
 app.listen(port, ()=>console.log(`i hear you ${port}`))
+
+app.post('/api/create', (req, res)=>{
+    json = req.body;
+    console.log(`json object: ${json}`)
+    res.sendStatus(200);
+})
+
+app.get('/api/read', (req, res)=>{
+    res.status(200).send(json);
+})
+
+app.put('/api/update', (req, res)=>{
+    json = Object.assign( {}, json, req.body );
+    res.sendStatus(200);
+})
+
+app.delete('/api/delete', (req, res)=>{
+    json = {};
+    res.sendStatus(200);
+})
--- src/App.js
+++ src/App.js
@@ -1,29 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      userInput: '',
+      status: '...',
+      json: {}
+    }
+  }
+  updateUserInput( e ) {
+    this.setState({ userInput: e.target.value })
+  }
+  create () {
+    this.setState({ status: '...' })
+    let json;
+    try {
+      json = JSON.parse(this.state.userInput)
+    }
+    catch (error) {
+      return this.setState({ status: 'invalid JSON format' })
+    }
+    
+    axios.post('/api/create', json)
+         .then( res=> this.setState({ status: `status: ${res.status} ${res.statusText}` }))
+    
+
+  }
+  read () {
+    this.setState({ status: '...' })
+    axios.get('/api/read')
+         .then( res=>{
+            this.setState({
+              json: res.data,
+              status: `status: ${res.status} ${res.statusText}`
+            })
+         })
+  }
+  update () {
+    this.setState({ status: '...' })
+    let json;
+    try {
+      json = JSON.parse(this.state.userInput)
+    }
+    catch (error) {
+      return this.setState({ status: 'invalid JSON format' })
+    }
+    axios.put('/api/update', json)
+         .then( res=> this.setState({ status: `status: ${res.status} ${res.statusText}` }))
+  }
+  delete () {
+    this.setState({ status: '...' })
+    axios.delete('/api/delete')
+         .then( res=> this.setState({ status: `status: ${res.status} ${res.statusText}` }))
+  }
   render() {
     return (
       <div className="app--component">
         <header>
           <h3> Axios Practice </h3>
           <div className="button--container">
-            <div className="button dark-color"> create </div>
-            <div className="button dark-color"> read </div>
-            <div className="button dark-color"> update </div>
-            <div className="button dark-color"> delete </div>
+            <div className="button dark-color" onClick={this.create.bind(this)}> create </div>
+            <div className="button dark-color" onClick={this.read.bind(this)}> read </div>
+            <div className="button dark-color" onClick={this.update.bind(this)}> update </div>
+            <div className="button dark-color" onClick={this.delete.bind(this)}> delete </div>
           </div>
         </header>
-        <section className="status">
+        <section className="status"> { this.state.status }
         </section>
         <main>
-          <textarea className="textarea"></textarea>
+          <textarea className="textarea" value={this.state.userInput} onChange={this.updateUserInput.bind(this)}></textarea>
 
           <div className="json--container">
 
-            <code></code>
+            <code>{ JSON.stringify(this.state.json, null, 4) }</code>
 
           </div>
         </main>
